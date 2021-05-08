9d71646ce2a6276e937a7ef8a47502a89ba2f4ce
Ave Nurme <nurme.ave@gmail.com>
2021-05-08 20:27:00 +0300

    feature: Build the 'add to cart' button functionality; create URl and view for the 'add' function; update the Basket class; add quantity to the session data.

--- basket/basket.py
+++ basket/basket.py
@@ -10,7 +10,20 @@ class Basket:
 
         # if the user does not have a session we will need to build one for the user
         if 'session_key' not in request.session:
-            basket = self.session['session_key'] = {'number': 1231231}
+            basket = self.session['session_key'] = {}
 
         # return the data of the session and store it in self.basket to make the variable available
         self.basket = basket
+
+    def add(self, product, qty):
+        """Adding and updating the users basket session data."""
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+
+        self.session.modified = True    # tell Django we have modified the session
+
+    def __len__(self):
+        """Get the basket data and count the qty of items."""
+        return sum(item['qty'] for item in self.basket.values())    # iterate through the basket and add quantities up
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from store.models import Product
+from .basket import Basket
 
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+
+def basket_add(request):
+    basket = Basket(request)    # grab the session data
+    if request.POST.get('action') == 'post':    # if the request received from the AJAX is a post request and action == post
+        product_id = int(request.POST.get('productid'))     # collect the product id
+        product_qty = int(request.POST.get('productqty'))   # collect the product quantity
+        product = get_object_or_404(Product, id=product_id)     # get the product from the database by id
+        basket.add(product=product, qty=product_qty)    # send/save the product data and quantity data into the session
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})     # send back the new updated basket quantity to the user
+        return response
