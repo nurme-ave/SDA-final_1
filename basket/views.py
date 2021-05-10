5037a9f2a8ee11282d085ae48df22a29484acf24
Ave Nurme <nurme.ave@gmail.com>
2021-05-10 23:46:06 +0300

    refactor: join 3 views into 1 view; modify 'action' attribute and url in Ajax request, join 3 paths into 1 path in urls

--- basket/views.py
+++ basket/views.py
@@ -10,40 +10,25 @@ def basket_summary(request):
     return render(request, 'store/basket/summary.html', {'basket': basket})
 
 
-def basket_add(request):
+def basket_action(request):
     basket = Basket(request)    # grab the session data
-    if request.POST.get('action') == 'post':    # if the request received from the AJAX is a post request and action == post
+
+    if request.POST.get('action') == 'add':    # if the request received from the AJAX is a post request and action == post
         product_id = int(request.POST.get('productid'))     # collect the product id
         product_qty = int(request.POST.get('productqty'))   # collect the product quantity
         product = get_object_or_404(Product, id=product_id)     # get the product from the database by id
         basket.add(product=product, qty=product_qty)    # send/save the product data and quantity data into the session
 
-        basketqty = basket.__len__()
-        baskettotal = basket.get_total_price()
-        response = JsonResponse({'qty': basketqty, 'total': baskettotal})     # send back the new updated basket quantity to the user
-        return response
+    elif request.POST.get('action') == 'delete':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
 
-
-def basket_delete(request):
-    basket = Basket(request)  # grab the session data
-    if request.POST.get('action') == 'post':  # if the request received from the AJAX is a post request and action == post
-        product_id = int(request.POST.get('productid'))  # collect the product id
-        basket.delete(product=product_id)  # send/save the product data and quantity data into the session
-
-        basketqty = basket.__len__()
-        baskettotal = basket.get_total_price()
-        response = JsonResponse({'qty': basketqty, 'total': baskettotal})  # send back the new updated basket quantity to the user
-        return response
-
-
-def basket_update(request):
-    basket = Basket(request)
-    if request.POST.get('action') == 'post':
+    elif request.POST.get('action') == 'update':
         product_id = int(request.POST.get('productid'))
         product_qty = int(request.POST.get('productqty'))
         basket.update(product=product_id, qty=product_qty)
 
-        basketqty = basket.__len__()
-        baskettotal = basket.get_total_price()
-        response = JsonResponse({'qty': basketqty, 'total': baskettotal})
-        return response
+    basket_qty = basket.__len__()
+    basket_total = basket.get_total_price()
+    response = JsonResponse({'qty': basket_qty, 'total': basket_total})     # send back the new updated basket quantity to the user
+    return response
