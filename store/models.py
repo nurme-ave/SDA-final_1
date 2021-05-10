75173b79092babb7285a2f309a08ee78e60eeb4f
Ave Nurme <nurme.ave@gmail.com>
2021-05-10 13:45:16 +0300

    refactor: remove 'size' field, add choices for colors

--- store/models.py
+++ store/models.py
@@ -24,11 +24,24 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
+    WHITE = 'wht'
+    BLACK = 'blc'
+    RED = 'red'
+    BLUE = 'blu'
+
+    COLOR_CHOICES = [
+        (WHITE, 'White'),
+        (BLACK, 'Black'),
+        (RED, 'Red'),
+        (BLUE, 'Blue'),
+    ]
+
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    size = models.IntegerField(default=0)
+    color = models.CharField(max_length=20, choices=COLOR_CHOICES, default=WHITE)
     image = models.ImageField(upload_to='images/', default='images/default.png')
     slug = models.SlugField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
