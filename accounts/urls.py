2e4dcb91397e343d41da3df399435e7218df791a
Ave Nurme <nurme.ave@gmail.com>
2021-05-14 18:15:38 +0300

    feature: finish forms styling and templates, add 'profile' link when logged in

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import MyLoginView, MySignUpView, MyPasswordChangeView
+
+from . import views
+from .views import MySignUpView, CustomerPasswordChangeView, CustomerProfileView, CustomerLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', MySignUpView.as_view(), name='signup'),
-    path('login/', MyLoginView.as_view(), name='login'),
+    path('login/', CustomerLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('password_change/', MyPasswordChangeView.as_view(), name='password_change'),
+    path('password_change/', CustomerPasswordChangeView.as_view(), name='password_change'),
+    path('password_change_success/', views.customer_password_change_success, name='password_change_success'),
+    path('customer_detail/<pk>/', CustomerProfileView.as_view(), name='customer_detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import SignUpForm
+from accounts.models import Profile
 
 
 class MySignUpView(CreateView):
@@ -11,10 +13,19 @@ class MySignUpView(CreateView):
     success_url = reverse_lazy("accounts:login")
 
 
-class MyLoginView(LoginView):
+class CustomerLoginView(LoginView):
     template_name = 'accounts/login.html'
 
 
-class MyPasswordChangeView(PasswordChangeView):
-    template_name = 'accounts/login.html'
-    success_url = reverse_lazy("store:all_products")
+class CustomerPasswordChangeView(PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+    success_url = reverse_lazy('accounts:password_change_success')
+
+
+def customer_password_change_success(request):
+    return render(request, 'accounts/password_change_success.html')
+
+
+class CustomerProfileView(DetailView):
+    template_name = 'accounts/profile.html'
+    model = Profile
